6ca53100cf124a2e8991039228fda04893527e37
SVNRiS3 <114239383+SVNRiS3@users.noreply.github.com>
2025-02-20 13:02:33 +0100

    Create fibonacci array recursively

--- fibonacci.js
+++ fibonacci.js
@@ -1,17 +1,23 @@
-function fibs(sequenceLength) {
-  const fibArray = [];
-  for (let i = 0; i < sequenceLength; i++) {
-    if (i < 2) fibArray.push(i);
-    else fibArray.push(fibArray[i - 1] + fibArray[i - 2]);
+// function fibs(sequenceLength) {
+//   const fibArray = [];
+//   for (let i = 0; i < sequenceLength; i++) {
+//     if (i < 2) fibArray.push(i);
+//     else fibArray.push(fibArray[i - 1] + fibArray[i - 2]);
+//   }
+//   return fibArray;
+// }
+
+function fibs(sequenceLength, fibArray = [0, 1]) {
+  if (sequenceLength === 1) {
+    return [0];
+  } else if (sequenceLength === 2) {
+    return [0, 1];
   }
+  fibArray.push(
+    fibs(sequenceLength - 1, fibArray).slice(-1)[0] +
+      fibs(sequenceLength - 2).slice(-1)[0]
+  );
   return fibArray;
 }
-
-// function fibs(sequenceLength) {
-//   if (sequenceLength < 2) {
-//     return sequenceLength;
-//   }
-//   return fibs(sequenceLength - 1) + fibs(sequenceLength - 2);
-// }
 
 console.log('Fib: ', fibs(8));
