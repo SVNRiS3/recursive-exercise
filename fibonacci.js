378f0fdd7b086dd2f42d1918e156fe59b95ea0ab
SVNRiS3 <114239383+SVNRiS3@users.noreply.github.com>
2025-02-20 12:12:07 +0100

    Create fibonacci array iteratively

--- fibonacci.js
+++ fibonacci.js
@@ -1,8 +1,17 @@
 function fibs(sequenceLength) {
-  if (sequenceLength < 2) {
-    return sequenceLength;
+  const fibArray = [];
+  for (let i = 0; i < sequenceLength; i++) {
+    if (i < 2) fibArray.push(i);
+    else fibArray.push(fibArray[i - 1] + fibArray[i - 2]);
   }
-  return fibs(sequenceLength - 1) + fibs(sequenceLength - 2);
+  return fibArray;
 }
 
-console.log(fibs(7));
+// function fibs(sequenceLength) {
+//   if (sequenceLength < 2) {
+//     return sequenceLength;
+//   }
+//   return fibs(sequenceLength - 1) + fibs(sequenceLength - 2);
+// }
+
+console.log('Fib: ', fibs(8));
