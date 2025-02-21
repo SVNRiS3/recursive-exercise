3a55a582143653530815c4af93888cf6b858e790
SVNRiS3 <114239383+SVNRiS3@users.noreply.github.com>
2025-02-21 09:12:14 +0100

    Simplify loop, add 2 item sorting

--- mergeSort.js
+++ mergeSort.js
@@ -1,21 +1,26 @@
 function mergeSort(arr) {
   if (arr.length < 2) {
     return arr;
-  };
+  }
 
   const leftArray = arr.slice(0, arr.length);
   const rightArray = arr.slice(arr.length, -1);
-  const tmp = [];
 
-  for (itemLeft, indexLeft in leftArray) {
-    for (itemRight, indexRight in rightArray) {
-      if (leftArray.length === 0) tmp = tmp.concat(rightArray);
-      if (rightArray.length === 0) tmp = tmp.concat(leftArray);
-      if (item > rightArray[0]) {
-        //todo
-      }
-    };
-  };
+  const tmp = [];
+  const i = 0;
+  const j = 0;
+
+  while (i < leftArray.length && j < rightArray.length) {
+    if (leftArray.length === i) tmp = tmp.concat(rightArray);
+    if (rightArray.length === j) tmp = tmp.concat(leftArray);
+    if (leftArray[i] > rightArray[j]) {
+      tmp.push(rightArray[j]);
+      j++;
+    } else if (leftArray[i] < rightArray[j]) {
+      tmp.push(leftArray[i]);
+      i++;
+    }
+  }
   const sortedArr = mergeSort(tmp);
 
   return sortedArr;
